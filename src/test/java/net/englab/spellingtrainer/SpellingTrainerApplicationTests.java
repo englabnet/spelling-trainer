f566f69ffc3b6767dd6e3796d7344f1d8a9e11d6
Nikita Kuchur <nikitakuchur@gmail.com>
2024-01-08 02:00:25 +0100

    fixed unit tests

--- src/test/java/net/englab/spellingtrainer/SpellingTrainerApplicationTests.java
+++ src/test/java/net/englab/spellingtrainer/SpellingTrainerApplicationTests.java
@@ -1,9 +1,7 @@
 package net.englab.spellingtrainer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpellingTrainerApplicationTests {
 
     @Test
